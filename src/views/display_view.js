0eaf304ca1c84efa538fec17fad19bd979e049cf
James <james___3248@hotmail.com>
2019-05-15 15:03:38 +0100

    completed extension

--- src/views/display_view.js
+++ src/views/display_view.js
@@ -22,8 +22,21 @@ DisplayView.prototype.render = function (countryObject) {
   flagImage.src = countryObject.flag;
   this.element.appendChild(flagImage);
 
-  const countryRegion = this.createElement('h2', countryObject.region);
+  const regionLabel = document.createElement('h2')
+  regionLabel.textContent = "Region:"
+  this.element.appendChild(regionLabel);
+
+  const countryRegion = this.createElement('h3', countryObject.region);
   this.element.appendChild(countryRegion);
+
+  const languageLabel = document.createElement('h2')
+  languageLabel.textContent = "Languages:"
+  this.element.appendChild(languageLabel);
+
+
+  const countryLanguages = countryObject.languages
+  const countryList = this.createListItem(countryLanguages);
+  this.element.appendChild(countryList);
 
 }
 
@@ -32,5 +45,19 @@ DisplayView.prototype.createElement = function(elementType, content){
   element.textContent = content;
   return element;
 }
+
+DisplayView.prototype.createListItem = function(countryLanguages){
+  const countryLanguageList = document.createElement('ul')
+
+  const countryList = countryLanguages.forEach((language) => {
+    const listItem = document.createElement('li')
+    listItem.textContent = language.name
+    countryLanguageList.appendChild(listItem);
+
+  })
+  return countryLanguageList;
+}
+
+
 
 module.exports = DisplayView;
