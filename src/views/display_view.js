3e9ca9187b47c81b4df139e437b545ff3a892306
James <james___3248@hotmail.com>
2019-05-15 14:44:27 +0100

    Displaying image and region

--- src/views/display_view.js
+++ src/views/display_view.js
@@ -7,6 +7,7 @@ const DisplayView = function (element) {
 DisplayView.prototype.bindEvents = function (){
   PubSub.subscribe('Countries:selected-country', (evt) => {
     const countryObject = evt.detail
+    console.log(countryObject);
     this.render(countryObject)
   })
 }
@@ -16,6 +17,14 @@ DisplayView.prototype.render = function (countryObject) {
 
   const countryName = this.createElement('h1', countryObject.name);
   this.element.appendChild(countryName);
+
+  const flagImage = new Image(200, 100)
+  flagImage.src = countryObject.flag;
+  this.element.appendChild(flagImage);
+
+  const countryRegion = this.createElement('h2', countryObject.region);
+  this.element.appendChild(countryRegion);
+
 }
 
 DisplayView.prototype.createElement = function(elementType, content){
